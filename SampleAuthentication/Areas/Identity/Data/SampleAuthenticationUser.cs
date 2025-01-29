7149fec94a7aacc8f91e3aa82ccd90cf86a12b21
F-Chitz <f.chitz@outlook.com>
2025-01-29 17:58:56 -0300

    Scaffolding Identity

---
+++ SampleAuthentication/Areas/Identity/Data/SampleAuthenticationUser.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace SampleAuthentication.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the SampleAuthenticationUser class
+public class SampleAuthenticationUser : IdentityUser
+{
+    [Required]
+    [MaxLength(200)]
+    public string FullName { get; set; } = string.Empty;
+
+    [MaxLength(50)]
+    public string PreferredName { get; set; }
+}
+
--- SampleAuthentication/Program.cs
+++ SampleAuthentication/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SampleAuthentication.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("SampleAuthenticationContextConnection") ?? throw new InvalidOperationException("Connection string 'SampleAuthenticationContextConnection' not found.");
+
+builder.Services.AddDbContext<SampleAuthenticationContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<SampleAuthenticationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<SampleAuthenticationContext>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
